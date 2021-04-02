63dee37855e7ad42dcc7d96d62c9ecf87a11da06
unknown <hccomputer1105.p@gmail.com>
2021-04-02 17:17:05 -0500

    primera modificacion de consultoria

--- consultoria.php
+++ consultoria.php
@@ -2,6 +2,56 @@
 include 'plantilla/cabecera.php'
 ?>
 
+<div class="row cabconsul">
+    <div class="col-8 textconsul">
+        <h1>consultoria organizacional</h1>
+        <p>en crecimiento con el cliente </p>
+    </div>
+    <div class="col-4 imgconsul">
+        <img src="imagenes/consultoria/gindo_apstrapto.jpg" alt="">
+    </div>
+</div>
+
+<div class="tarjeta">
+    <div class="tarjeta-uno">
+        <div class="tarjeta-primera">
+            <div class="adelante">
+
+            </div>
+            <div class="atras">
+                prueba de
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="tarjeta">
+    <div class="tarjeta-uno">
+        <div class="tarjeta-primera">
+            <div class="adelante">
+
+            </div>
+            <div class="atras">
+                prueba de
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="tarjeta">
+    <div class="tarjeta-uno">
+        <div class="tarjeta-primera">
+            <div class="adelante">
+
+            </div>
+            <div class="atras">
+                prueba de
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 
 <!-- PIE DE PAGINA  -->
