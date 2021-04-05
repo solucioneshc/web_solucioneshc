3f52ac8a6c0af00e09af6df8aabf99647aa17c02
HECTOR <hccomputer1105.p@gmail.com>
2021-04-05 12:28:29 -0500

    trabajando en consultoria

--- consultoria.php
+++ consultoria.php
@@ -2,7 +2,7 @@
 include 'plantilla/cabecera.php'
 ?>
 
-<div class="row cabconsul">
+<div class="row cabconsul ">
     <div class="col-8 textconsul">
         <h1>consultoria organizacional</h1>
         <p>en crecimiento con el cliente </p>
@@ -12,44 +12,60 @@ include 'plantilla/cabecera.php'
     </div>
 </div>
 
-<div class="tarjeta">
-    <div class="tarjeta-uno">
+
+
+<div class="tarjeta d-sm-flex d-md-flex d-lg-flex d-xl-flex d-flex ">
+    <div class="tarjeta-uno ">
         <div class="tarjeta-primera">
-            <div class="adelante">
+            <div class="adelante card1">
 
             </div>
             <div class="atras">
-                prueba de
+                Consultoria organizacional
             </div>
         </div>
     </div>
-
-</div>
-<div class="tarjeta">
-    <div class="tarjeta-uno">
+    <div class="tarjeta-uno ">
         <div class="tarjeta-primera">
-            <div class="adelante">
+            <div class="adelante card2">
 
             </div>
             <div class="atras">
-                prueba de
+                innovacion estrategica
             </div>
         </div>
+
+
     </div>
 
-</div>
-<div class="tarjeta">
-    <div class="tarjeta-uno">
+    <div class="tarjeta-uno ">
         <div class="tarjeta-primera">
-            <div class="adelante">
+            <div class="adelante card3">
 
             </div>
             <div class="atras">
-                prueba de
+                Marketing
             </div>
         </div>
+
     </div>
+    <div class="tarjeta-uno  ">
+        <div class="tarjeta-primera">
+            <div class="adelante card4">
+
+            </div>
+            <div class="atras">
+                Transformacion Digital
+            </div>
+        </div>
+        
+
+    </div>
+
 </div>
+
+
+
 
 
 
