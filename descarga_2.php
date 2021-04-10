2a1228ea9ca33ca67a0971ffdd3f7a0cd3b23e2d
unknown <hccomputer1105.p@gmail.com>
2021-04-10 00:09:14 -0500

    correccion descarga tamaño imagen y texto

--- descarga_2.php
+++ descarga_2.php
@@ -45,7 +45,7 @@ include 'plantilla/cabecera.php';
 
                 <?php foreach($listaProductos as $producto){ ?>
 
-                <div class="col-md-4 col-sm-6 col-xs-12 mt-3">
+                <div class="ml-3 grid_descargas">
                     <div class="card">
                         <img title=<?php echo $producto ['nombre'];?> alt=<?php echo $producto ['nombre'];?>
                             class="card-img-top" data-toggle="popover" data-trigger="hover"
